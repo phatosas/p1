54bec447eccae4a94f4acc4bca761223ec29ab58
phatosas <phatosas@yahoo.com>
2016-02-12 10:20:59 -0500

    Added README, image and index file

---
+++ README.md
@@ -0,0 +1,18 @@
+# Project One
+
+## Live URL
+<http://p1.dwa15.com>
+
+## Description
+A single page website using basic HTML and CSS with the following information:
+Name
+Photo
+Place holder for future projects
+
+## Demo
+<http://screencast.com/t/oUoSnN29>
+
+## Details
+No login required.
+
+The file `word-scrape.php` was used to generate the word list.
---
+++ index.php
@@ -0,0 +1,50 @@
+<html>
+  <head>
+	<title>Osagie Imafidon</title>
+	<style type="text/css">
+		body{
+		     background-color: #c0c0c0;
+		     margin-left: 20%
+		     margin-right: 20%
+             border: 1px dotted grey;
+		     padding: 10px 10px 10px 10px;
+             font-family: "Times New Roman";
+		}
+		h1{
+            color: green;
+            text-align: center;
+        }
+		h2{
+		   color: blue;
+		   text-align: center;
+		}
+        p{
+            text-align: center;
+        }
+	</style>
+  </head>
+  <body>
+	<h1>Osagie Imafidon</h1>
+	<p>
+		<b>cscie15 project page</b>
+	</p>
+	<p>
+	<img src="images/osagie.jpg">
+	</p>
+	<h2>Project 2 Title</h2>
+	<p>
+	View:[link]<br>
+	Github:[link]<br>
+	</p>
+	<h2>Project 3 Title</h2>
+	<p>
+	View:[link]<br>
+	Github:[link]<br>
+	</p>
+	<h2>Project 4 Title</h2>
+	<p>
+	View:[link]<br>
+	Github:[link]<br>
+	</p>
+  </body>
+</html>	
